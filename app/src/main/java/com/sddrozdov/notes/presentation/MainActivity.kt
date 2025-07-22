ec48434b39c235aa898e1f70ee4b58bccf7451ff
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-07-22 16:22:39 +0300

    added createNote screen

--- app/src/main/java/com/sddrozdov/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/sddrozdov/notes/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.sddrozdov.notes.presentation.screens.creation.CreateNoteScreen
 import com.sddrozdov.notes.presentation.screens.notes.NoteScreen
 import com.sddrozdov.notes.presentation.ui.theme.NotesTheme
 
@@ -13,14 +14,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NotesTheme {
-                NoteScreen(
-                    onNoteClick = {
-
-                },
-                    onAddNoteClick = {
-
-                    }
-                )
+                CreateNoteScreen()
+//                NoteScreen(
+//                    onNoteClick = {
+//
+//                },
+//                    onAddNoteClick = {
+//
+//                    }
+//                )
             }
         }
     }
